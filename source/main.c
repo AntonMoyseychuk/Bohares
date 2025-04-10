c1e9c3a9fb9f427f2ff5cc5fc0745464f6273736
Anton Moyseychuk <moyseychuk2017@mail.ru>
2025-04-10 23:58:57 +0400

    - Implemented colored output

--- source/main.c
+++ source/main.c
@@ -4,11 +4,24 @@
 #include "utils/file/file.h"
 
 
+#define BOH_OUTPUT_COLOR_RESET_ASCII_CODE      "\033[0m"
+#define BOH_OUTPUT_COLOR_BLACK_ASCII_CODE      "\033[30m"
+#define BOH_OUTPUT_COLOR_RED_ASCII_CODE        "\033[31m"
+#define BOH_OUTPUT_COLOR_GREEN_ASCII_CODE      "\033[32m"
+#define BOH_OUTPUT_COLOR_YELLOW_ASCII_CODE     "\033[33m"
+#define BOH_OUTPUT_COLOR_BLUE_ASCII_CODE       "\033[34m"
+#define BOH_OUTPUT_COLOR_MAGENTA_ASCII_CODE    "\033[35m"
+#define BOH_OUTPUT_COLOR_CYAN_ASCII_CODE       "\033[36m"
+#define BOH_OUTPUT_COLOR_WHITE_ASCII_CODE      "\033[37m"
+
+#define BOH_MAKE_COLORED_TEXT(text, color) color text BOH_OUTPUT_COLOR_RESET_ASCII_CODE
+
+
 int main(int argc, char* argv[])
 {
 // #define DEBUG_NO_ARGS
 #ifdef DEBUG_NO_ARGS
-    const char* str = "#[ if x >= 0 then print(\"x is positive\n\") else print(\"x is negative\n\") end]#";
+    const char* str = "if x >= 0 then print(\"x is positive\") else print(\"x is negative\") end";
 
     bohLexer lexer = bohLexerCreate(str, strlen(str) + 1);
 
@@ -18,10 +31,12 @@ int main(int argc, char* argv[])
     for (size_t i = 0; i < tokensCount; ++i) {
         const bohToken* pToken = bohTokenStorageAt(&tokens, i);
 
-        const bohString* pLexeme = bohTokenGetLexeme(pToken);
-        const char* pLexemeStr = bohStringGetCStr(pLexeme);
+        const bohStringView lexeme = bohTokenGetLexeme(pToken);
+        const char* pLexemeStr = bohStringViewGetData(&lexeme);
+        const size_t lexemeSize = bohStringViewGetSize(&lexeme);
 
-        fprintf_s(stdout, "%s\t(line: %u, col: %u): %s\n", bohTokenGetTypeStr(pToken), pToken->line, pToken->column, pLexemeStr);
+        fprintf_s(stdout, "(" BOH_MAKE_COLORED_TEXT("%s", BOH_OUTPUT_COLOR_YELLOW_ASCII_CODE) ", " BOH_MAKE_COLORED_TEXT("%.*s", BOH_OUTPUT_COLOR_GREEN_ASCII_CODE) ", %u, %u)\n", 
+            bohTokenGetTypeStr(pToken), lexemeSize, pLexemeStr, pToken->line, pToken->column);
     }
 
     return EXIT_SUCCESS;
@@ -48,7 +63,8 @@ int main(int argc, char* argv[])
         const char* pLexemeStr = bohStringViewGetData(&lexeme);
         const size_t lexemeSize = bohStringViewGetSize(&lexeme);
 
-        fprintf_s(stdout, "%s\t(line: %u, col: %u): %.*s\n", bohTokenGetTypeStr(pToken), pToken->line, pToken->column, lexemeSize, pLexemeStr);
+        fprintf_s(stdout, "(" BOH_MAKE_COLORED_TEXT("%s", BOH_OUTPUT_COLOR_YELLOW_ASCII_CODE) ", " BOH_MAKE_COLORED_TEXT("%.*s", BOH_OUTPUT_COLOR_GREEN_ASCII_CODE) ", %u, %u)\n", 
+            bohTokenGetTypeStr(pToken), lexemeSize, pLexemeStr, pToken->line, pToken->column);
     }
 
     bohFileContentFree(&fileContent);
